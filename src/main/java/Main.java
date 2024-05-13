a92b760414bf33c3fec2fd4c13ba28e22991bb11
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-05-13 14:59:04 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,29 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("///////////////////");
-    System.out.println(" / Banda: Deftones / ");
+    System.out.println("/ Banda: Deftones /");
     System.out.println("///////////////////");
 
 
     System.out.println("");
 
-    System.out.println("Album : Ohms ");
-      System.out.println("Ano de lançamento: 2020");
+    System.out.println("Album : White pony ");
+      System.out.println("Ano de lançamento: 2000");
 
-  System.out.println(" 'Ohms' ");
+  System.out.println(" White Pony é o terceiro álbum de estúdio da banda americana de metal alternativo Deftones, lançado em 20 de junho de 2000, pela Maverick Records. Foi produzido por Terry Date, que produziu os dois primeiros álbuns da banda, Adrenaline e Around the Fur.   ");
+System.out.println("------------------------------------------------------------------------------------------------");
+  System.out.println("");
     
   System.out.println("///////////");
-  System.out.println(" / Musicas / ");
+  System.out.println("/ Musicas /");
   System.out.println("///////////");
 
-    
-    
+   System.out.println(" 1 Back to school     3:58."); 
+    System.out.println(" 2 Feiticeira         3:10.");
+    System.out.println(" 3 Digital Bath       4:16.");
+    System.out.println(" 4 Elite             04:02.");
+    System.out.println(" 5 Rx Queen          04:29.");
+    System.out.println(" 6 Steet Carp        02:42.");
     
   }
 

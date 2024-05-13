f2b171e4ee1bb9890ca78aaa9d940b8334a23870
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-05-13 14:37:45 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("///////////////////");
+    System.out.println(" / Banda: Deftones / ");
+    System.out.println("///////////////////");
+
+
+    System.out.println("");
+
+    System.out.println("Album : Ohms ");
+      System.out.println("Ano de lançamento: 2020");
+
+  System.out.println(" 'Ohms' ");
+    
+  System.out.println("///////////");
+  System.out.println(" / Musicas / ");
+  System.out.println("///////////");
+
+    
+    
+    
+  }
+
+  
+
+  
+}
